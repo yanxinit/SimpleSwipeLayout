c3582f665d86ff0f8ea16cdac04a2bf4fd79d803
YanXin <yanxin@loopeer.com>
2015-12-29 17:38:48 +0800

    add divider in recycler demo

--- app/src/main/java/com/yanxin/library/simpleswipelayoutdemo/RecyclerViewActivity.java
+++ app/src/main/java/com/yanxin/library/simpleswipelayoutdemo/RecyclerViewActivity.java
@@ -20,6 +20,7 @@ public class RecyclerViewActivity extends AppCompatActivity {
         setContentView(R.layout.recyclerview);
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this));
         final String[] adapterData = new String[]{"Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"};
         mAdapter = new RecyclerView.Adapter<MyViewHolder>() {
             @Override
